921e8e798d41cb90e10daea07416263d118f9160
Aleksandr Sventorzhycky <a.o.absolute.zero@gmail.com>
2020-03-18 02:41:13 +0300

    add 3 components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header className='header'>
+        <img src="https://png.pngtree.com/element_pic/16/05/30/11574bb301599cc.jpg" alt=""/>
+    </header>
+    );
+};
+
+export default Header;
