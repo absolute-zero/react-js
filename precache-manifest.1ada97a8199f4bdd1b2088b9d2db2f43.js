a7225a314c4560f11b946a0a6aeb37461ec9ff45
Aleksandr Sventorzhycky <a.o.absolute.zero@gmail.com>
2020-10-07 19:54:26 +0300

    Updates

---
+++ precache-manifest.1ada97a8199f4bdd1b2088b9d2db2f43.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9c2d6cfe1c1350923be97fe24f63b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c53451afc2b7fb90449",
+    "url": "/static/css/3.884ab99a.chunk.css"
+  },
+  {
+    "revision": "df6773a11b20f5e5c5ad",
+    "url": "/static/css/5.a9088c62.chunk.css"
+  },
+  {
+    "revision": "29df483eeda5265a7cab",
+    "url": "/static/css/6.097a5e1a.chunk.css"
+  },
+  {
+    "revision": "13a745e66f22f883569f",
+    "url": "/static/css/main.7a29b4a8.chunk.css"
+  },
+  {
+    "revision": "570abe44ad0e27682690",
+    "url": "/static/js/2.026e41f5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.026e41f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c53451afc2b7fb90449",
+    "url": "/static/js/3.2913134e.chunk.js"
+  },
+  {
+    "revision": "712771d0e584ab0136ed",
+    "url": "/static/js/4.37026d78.chunk.js"
+  },
+  {
+    "revision": "df6773a11b20f5e5c5ad",
+    "url": "/static/js/5.33f9e23c.chunk.js"
+  },
+  {
+    "revision": "29df483eeda5265a7cab",
+    "url": "/static/js/6.203e9370.chunk.js"
+  },
+  {
+    "revision": "13a745e66f22f883569f",
+    "url": "/static/js/main.029c0618.chunk.js"
+  },
+  {
+    "revision": "d7f7a13da74787ad22e2",
+    "url": "/static/js/runtime-main.6be3113b.js"
+  },
+  {
+    "revision": "dc50c71bff854f41fc3f24eb7e9882ef",
+    "url": "/static/media/Spinner.dc50c71b.gif"
+  },
+  {
+    "revision": "4da2329d66bdfc278dba895ac0b57976",
+    "url": "/static/media/fontello.4da2329d.ttf"
+  },
+  {
+    "revision": "a903e122b19df04f0b82ec42be873e47",
+    "url": "/static/media/fontello.a903e122.eot"
+  },
+  {
+    "revision": "b46a3a2d0c59d9fb2a76f85ca488c411",
+    "url": "/static/media/fontello.b46a3a2d.woff"
+  },
+  {
+    "revision": "ea7f499e9b31d307feb8aeba0857a544",
+    "url": "/static/media/fontello.ea7f499e.svg"
+  },
+  {
+    "revision": "215d2729880b1c3ab4b44e530945887f",
+    "url": "/static/media/user.215d2729.jpg"
+  }
+]);
